49ad55a00ebd42ec9d5b2a8fccf5d30fea54c6a1
Vadim Gorbunov <vad.gorbunov@gmail.com>
2016-11-19 12:03:46 +0300

    Add deals list

--- truffle.js
+++ truffle.js
@@ -2,15 +2,23 @@ module.exports = {
   build: {
     "index.html": "index.html",
     "app.js": [
-      "javascripts/app.js"
+      "bower_components/jquery/dist/jquery.min.js",
+      "bower_components/bootstrap/dist/js/bootstrap.min.js",
+      "bower_components/angular/angular.min.js",
+      "bower_components/angular-route/angular-route.min.js",
+      "javascripts/app.js",
+      "apps/dashboard/controllers/dashboard.js"
     ],
     "app.css": [
+      "bower_components/bootstrap/dist/css/bootstrap.min.css",
+      "bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
       "stylesheets/app.css"
     ],
-    "images/": "images/"
+    "images/": "images/",
+    "apps/": "apps/"
   },
   rpc: {
-    host: "localhost",
+    host: "78.155.207.180",
     port: 8545
   }
 };
