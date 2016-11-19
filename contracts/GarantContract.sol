6231e38ce6780a9641ffb68c6aceb0d7db3ab67a
enspaender <andrey.leontiev.aiesec@gmail.com>
2016-11-19 23:35:01 +0300

    RABOTAET WATCHER

--- contracts/GarantContract.sol
+++ contracts/GarantContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.4;
 
 import "BankContract.sol";
 import "BuyerContract.sol";
@@ -6,5 +6,41 @@ import "SellerContract.sol";
 import "WatcherContract.sol";
 
 contract GarantContract{
+    address public owner;
+    address public seller;
+    address public bank;
+    address public buyer;
+    address public watcher;
     
+    
+    
+    
+    function createSeller() returns(address SellerAddr){
+        return address(new SellerContract());
+    }
+
+    
+    function createSBuyer() returns(address BuyerAddr){
+        return address(new BuyerContract());
+    }
+    
+    
+    function createBank() returns(address BankAddr){
+        return address(new BankContract());
+    }
+    
+    
+    
+    function createWatcher() returns(address WatcherAddr){
+        return address(new WatcherContract());
+    }
+
+    function GarantContract(){
+        owner = msg.sender;
+        seller = createSeller();
+        bank = createBank();
+        buyer = createSBuyer();
+        watcher = createWatcher();
+    }
+   
 }
--- contracts/WatcherContract.sol
+++ contracts/WatcherContract.sol
@@ -1,5 +1,31 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.4;
 
 contract WatcherContract{
+    bool check = true;
+    uint stop = 0;
+    uint random_number;
+    uint result;
+
+    function checkingStatus() returns(uint val){
+        while(check == true){
+            if(stop == 40){
+                random_number = uint(block.blockhash(block.number-1))%100 + 1;
+                return random_number;
+            }
+            stop++;
+            
+        }
+        
+        
+
+    }
     
+    function WatcherContract(){
+        result = checkingStatus();
+        if(result > 50){
+            //Обратить к какому то методу банка хД
+        }else{
+            //послать всё и сидеть
+        }
+    }
 }
